3c6b9da1ebd2433daa0b311fc4c0649564917da9
悦　結希 <xzuzx.9.xzuzx@ezweb.ne.jp>
2017-10-17 17:38:41 +0900

    課題　変数の宣言と表示（四則演算）を提出します。

---
+++ CampChallenge_PHP/003.四則演算/四則演算.php
@@ -0,0 +1,25 @@
+<?php
+const num = 100;
+$num2 = 10;
+$num3 = 90;
+//足し算
+echo $tasu = num+$num2;
+
+//引き算
+echo $hiku = num-$num2;
+
+//掛け算
+echo $kake = num*$num2;
+
+//割り算
+echo $wari = num/$num2;
+
+//剰余算
+echo $joyo = num%$num3;
+
+//加算子
+echo ++$num2;
+
+//減算子
+echo --$num3;
+ ?>
